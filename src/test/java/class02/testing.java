4f27d5f2de6c81c3a8ece93d523cb87af9296ec1
moazzam <moazzamsadiq94@gmail.com>
2022-05-10 20:29:09 -0400

    class02

--- src/test/java/class02/testing.java
+++ src/test/java/class02/testing.java
@@ -1,6 +1,15 @@
 package class02;
 
+import org.testng.annotations.Test;
+
 public class testing {
 
-    skajdlksajdkljsalkdfjlksafkjdalk
+ @Test
+    public void Test(){
+    System.out.println("drive me crazy , drive me mad");
+        }
+    @Test
+    public void Test2(){
+        System.out.println("drive me to sanity , drive me to hell");
+    }
 }
---
+++ src/test/java/class02/testing2.java
@@ -0,0 +1,14 @@
+package class02;
+
+import org.testng.annotations.Test;
+
+public class testing2 {
+    @Test
+    public void Testb(){
+        System.out.println("drive me 888888 , drive me888888");
+    }
+    @Test
+    public void Testa(){
+        System.out.println("drive me to 999999 , drive me to 999999");
+    }
+}
